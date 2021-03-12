d67c1b9a153699481430fa1c00362b5504f0bb0f
Kuba Szostko <kubaszostko@gmail.com>
2021-03-12 17:53:25 +0100

    created attraction model

--- travelapp/attractions/models.py
+++ travelapp/attractions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Attraction(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    # maybe list of images later on
+    photo = models.ImageField()
+
+    class Meta:
+        ordering = ['name']
