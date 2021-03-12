4909c6e34c0b819e45a687fedf23100a817ff66e
Kuba Szostko <kubaszostko@gmail.com>
2021-03-12 18:10:32 +0100

    created ViewSet for Attraction model

--- travelapp/attractions/models.py
+++ travelapp/attractions/models.py
@@ -5,7 +5,8 @@ class Attraction(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     # maybe list of images later on
-    photo = models.ImageField()
+    # TODO: add support for images
+    # photo = models.ImageField()
 
     class Meta:
         ordering = ['name']
--- travelapp/attractions/views.py
+++ travelapp/attractions/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Attraction
+from .serializers import AttractionSerializer
 
-# Create your views here.
+
+class AttractionViewSet(viewsets.ModelViewSet):
+    queryset = Attraction.objects.all()
+    serializer_class = AttractionSerializer
+    # TODO: think about proper permissions
+    permission_classes = [permissions.AllowAny]
