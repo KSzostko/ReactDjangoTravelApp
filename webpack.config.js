3ee529f8ad859021e4172b234c840d620b974bf5
Kuba Szostko <kubaszostko@gmail.com>
2021-03-13 11:07:09 +0100

    added file-loder in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,17 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
-}
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
