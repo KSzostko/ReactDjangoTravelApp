447c71c02e38c5f6a6ac323ccebb80765416f834
Kuba Szostko <kubaszostko@gmail.com>
2021-03-13 11:09:10 +0100

    url in photo field of Attraction model points to the image now

--- travelapp/attractions/serializers.py
+++ travelapp/attractions/serializers.py
@@ -3,13 +3,6 @@ from .models import Attraction
 
 
 class AttractionSerializer(serializers.ModelSerializer):
-    photo = serializers.SerializerMethodField()
-
-    # not sure whether received url will be enough
-    # will find out after trying to use the data
-    def get_photo(self, instance):
-        return instance.photo.url
-
     class Meta:
         model = Attraction
         fields = ('name', 'description', 'photo')
