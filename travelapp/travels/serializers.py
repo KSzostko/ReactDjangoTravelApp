d37ff984fe1428a6fc24ea9e9581f6b012b3ae0f
Kuba Szostko <kubaszostko@gmail.com>
2021-03-14 13:02:25 +0100

    travel-images can now be filtered by travel id

--- travelapp/travels/serializers.py
+++ travelapp/travels/serializers.py
@@ -3,11 +3,9 @@ from .models import Travel, TravelImage
 
 
 class TravelSerializer(serializers.ModelSerializer):
-    images = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
     class Meta:
         model = Travel
-        fields = ['id', 'name', 'short_description', 'description', 'start_date', 'end_date', 'transport', 'hotel', 'attractions', 'images']
+        fields = '__all__'
         depth = 1
 
 
--- travelapp/travels/views.py
+++ travelapp/travels/views.py
@@ -16,3 +16,15 @@ class TravelImageViewSet(viewsets.ModelViewSet):
     serializer_class = TravelImageSerializer
     # TODO: think about proper permissions
     permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        try:
+            travel_id = int(self.request.query_params.get('travel-id'))
+
+            return qs.filter(travel_id=travel_id)
+        except TypeError:
+            return qs
+        except ValueError:
+            return qs
