f5a17a4fbc45535d0c03065b954bcb89f34386f5
Kuba Szostko <kubaszostko@gmail.com>
2021-06-24 11:54:22 +0200

    feat: test api connection in the frontend app

--- travelapp/frontend/src/App.js
+++ travelapp/frontend/src/App.js
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
+import axios from 'axios';
 import { Button } from 'antd';
 
 function App() {
+  useEffect(() => {
+    async function testFetch() {
+      await axios
+        .get('http://localhost:8000/api/hotels/')
+        .then(({ data }) => {
+          console.log(data);
+        })
+        .catch(({ message }) => {
+          console.error(message);
+        });
+    }
+
+    testFetch();
+  });
+
   return (
     <div>
       <Button>Hello world</Button>
