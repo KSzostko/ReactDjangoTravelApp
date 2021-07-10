780baa67eed44534caa7ba070f770f61190659a8
Kuba Szostko <kubaszostko@gmail.com>
2021-07-10 17:40:38 +0200

    fix(placesapi): handle error and valid reponses

--- travelapp/frontend/src/services/places/placesAPI.js
+++ travelapp/frontend/src/services/places/placesAPI.js
@@ -10,25 +10,30 @@ const findPlacesInRadius = ({
   lon,
   categoriesList = getAllCategories(),
 }) =>
-  axios.get(
-    `${BASE_URL}/radius?
+  axios
+    .get(
+      `${BASE_URL}/radius?
       &radius=${radius}&lat=${lat}&lon=${lon}
       &kinds=${categoriesList.toString()}&format=json
       &apikey=${process.env.REACT_APP_OPEN_TRIP_MAP_API_KEY}`
-  );
+    )
+    .then(({ data }) => data);
 
 const findPlacesInBox = ({
   latRange,
   lonRange,
   categoriesList = getAllCategories(),
 }) =>
-  axios.get(
-    `${BASE_URL}/bbox?
+  axios
+    .get(
+      `${BASE_URL}/bbox?
         &lat_min=${latRange.min}&lat_max=${latRange.max}
         &lon_min=${lonRange.min}&lon_max=${lonRange.max}
         &kinds=${categoriesList.toString()}&format=json
         &apikey=${process.env.REACT_APP_OPEN_TRIP_MAP_API_KEY}`
-  );
+    )
+    .then(({ data }) => data)
+    .catch(({ response }) => Promise.reject(response.data.error));
 
 const findClosestSuggestions = ({
   searchTerm,
@@ -37,18 +42,24 @@ const findClosestSuggestions = ({
   radius,
   categoriesList = getAllCategories(),
 }) =>
-  axios.get(
-    `${BASE_URL}/autosuggest?
+  axios
+    .get(
+      `${BASE_URL}/autosuggest?
       &name=${searchTerm}&lat=${lat}&lon=${lon}&radius=${radius}
       &kinds=${categoriesList.toString()}&format=json
       &apikey=${process.env.REACT_APP_OPEN_TRIP_MAP_API_KEY}`
-  );
+    )
+    .then(({ data }) => data)
+    .catch(({ response }) => Promise.reject(response.data.error));
 
 const getPlaceDetails = (xid) =>
-  axios.get(
-    `${BASE_URL}/xid/${xid}
+  axios
+    .get(
+      `${BASE_URL}/xid/${xid}
         ?apikey=${process.env.REACT_APP_OPEN_TRIP_MAP_API_KEY}`
-  );
+    )
+    .then(({ data }) => data)
+    .catch(({ response }) => Promise.reject(response.data.error));
 
 export default {
   findPlacesInRadius,
