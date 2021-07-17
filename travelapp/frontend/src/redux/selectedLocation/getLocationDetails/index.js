8b0e1538a1f38c0b3472d9f331e1c3eece4abe5f
Kuba Szostko <kubaszostko@gmail.com>
2021-07-17 09:24:38 +0200

    feat: add isModalOpen filed to selected location store

--- travelapp/frontend/src/redux/selectedLocation/getLocationDetails/index.js
+++ travelapp/frontend/src/redux/selectedLocation/getLocationDetails/index.js
@@ -3,6 +3,7 @@ import { getLocationsDetails } from './thunk';
 export const getLocationDetailsReducer = (builder) => {
   builder.addCase(getLocationsDetails.pending, (state) => {
     state.isLoading = true;
+    state.isModalOpen = true;
     state.error = null;
   });
 
@@ -13,6 +14,7 @@ export const getLocationDetailsReducer = (builder) => {
 
   builder.addCase(getLocationsDetails.rejected, (state, action) => {
     state.isLoading = false;
+    state.isModalOpen = false;
     state.data = null;
     state.error = action.payload;
   });
--- travelapp/frontend/src/redux/selectedLocation/selectedLocationSlice.js
+++ travelapp/frontend/src/redux/selectedLocation/selectedLocationSlice.js
@@ -4,16 +4,26 @@ import { getLocationDetailsReducer } from './getLocationDetails';
 const initialState = {
   data: null,
   isLoading: false,
+  isModalOpen: false,
   error: null,
 };
 
 const selectedLocationSlice = createSlice({
   name: 'selectedLocation',
   initialState,
-  reducers: {},
+  reducers: {
+    clearLocationData(state) {
+      state.data = null;
+      state.isLoading = false;
+      state.isModalOpen = false;
+      state.error = null;
+    },
+  },
   extraReducers: (builder) => {
     getLocationDetailsReducer(builder);
   },
 });
+
+export const { clearLocationData } = selectedLocationSlice.actions;
 
 export default selectedLocationSlice.reducer;
