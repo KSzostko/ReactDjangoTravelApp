76ca9ed57c55c4921fc28df883f0296ffc79496e
Kuba Szostko <kubaszostko@gmail.com>
2021-07-27 16:53:58 +0200

    feat: add travel stop modal to the store

--- travelapp/frontend/src/redux/store.js
+++ travelapp/frontend/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
 import mapReducer from './map/mapSlice';
 import selectedLocationReducer from './selectedLocation/selectedLocationSlice';
+import travelStopModalReducer from './travelStopModal/travelStopModalSlice';
 
 const rootReducer = combineReducers({
   user: userReducer,
   map: mapReducer,
   selectedLocation: selectedLocationReducer,
+  travelStopModal: travelStopModalReducer,
 });
 
 export const store = configureStore({
---
+++ travelapp/frontend/src/redux/travelStopModal/travelStopModalSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  data: null,
+  isOpen: false,
+};
+
+const selectedLocationSlice = createSlice({
+  name: 'travelStopModal',
+  initialState,
+  reducers: {
+    closeModal(state) {
+      state.isOpen = false;
+    },
+    chooseTravelStop(state, action) {
+      state.isOpen = true;
+      state.data = action.payload;
+    },
+  },
+});
+
+export const { closeModal, chooseTravelStop } = selectedLocationSlice.actions;
+
+export default selectedLocationSlice.reducer;
