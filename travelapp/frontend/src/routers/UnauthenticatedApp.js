66329dd1b7b1fb2ca7dd2901f5369d8893036749
Kuba Szostko <kubaszostko@gmail.com>
2021-07-31 17:19:52 +0200

    fix: logged user is not redirected to the main page after reload

--- travelapp/frontend/src/routers/UnauthenticatedApp.js
+++ travelapp/frontend/src/routers/UnauthenticatedApp.js
@@ -3,12 +3,14 @@ import LoginView from '../views/LoginView';
 import RegisterView from '../views/RegisterView';
 
 function UnauthenticatedApp() {
+  const shouldRedirect = localStorage.getItem('token') === null;
+
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={LoginView} />
         <Route path="/register" component={RegisterView} />
-        <Redirect to="/" />
+        {shouldRedirect && <Redirect to="/" />}
       </Switch>
     </BrowserRouter>
   );
