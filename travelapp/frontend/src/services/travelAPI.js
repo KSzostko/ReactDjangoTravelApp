957ffb10b5ac840add6951b34d2bd5999ae01e02
Kuba Szostko <kubaszostko@gmail.com>
2021-08-21 09:50:48 +0200

    feat(travelAPI): get travel by id

--- travelapp/frontend/src/services/travelAPI.js
+++ travelapp/frontend/src/services/travelAPI.js
@@ -2,15 +2,25 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:8000/api/travels/';
 
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: `Token ${localStorage.getItem('token')}`,
+};
+
 const create = (travelData) =>
   axios
     .post(BASE_URL, travelData, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Token ${localStorage.getItem('token')}`,
-      },
+      headers,
     })
     .then(({ data }) => data)
     .catch(({ response }) => Promise.reject(response.statusText));
 
-export default { create };
+const getById = (id) =>
+  axios
+    .get(`${BASE_URL}${id}`, {
+      headers,
+    })
+    .then(({ data }) => data)
+    .catch(({ response }) => Promise.reject(response.statusText));
+
+export default { create, getById };
