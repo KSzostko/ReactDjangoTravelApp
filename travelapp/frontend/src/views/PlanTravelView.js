f3f2e1957321ad9ac8592edd21010776e87dcf33
Kuba Szostko <kubaszostko@gmail.com>
2021-08-21 10:21:04 +0200

    feat(PlanTravelView): get travel data after reload

--- travelapp/frontend/src/views/PlanTravelView.js
+++ travelapp/frontend/src/views/PlanTravelView.js
@@ -1,12 +1,30 @@
+import { useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Spin } from 'antd';
+import { getTravelById } from '../redux/travels/actions/getTravelById/thunk';
 import AuthLayout from '../components/AuthLayout';
 import Map from '../components/Map/Map';
 
 function PlanTravelView() {
-  return (
-    <AuthLayout mapView>
-      <Map />
-    </AuthLayout>
-  );
+  const history = useHistory();
+  const { travelId } = useParams();
+
+  const dispatch = useDispatch();
+  const { data, isLoading } = useSelector((state) => state.travels.current);
+
+  useEffect(() => {
+    if (data !== null) return;
+
+    dispatch(getTravelById(travelId))
+      .unwrap()
+      .catch(() => {
+        history.push('/travel/start');
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch]);
+
+  return <AuthLayout mapView>{isLoading ? <Spin /> : <Map />}</AuthLayout>;
 }
 
 export default PlanTravelView;
