846f8c6f1887a855452fc2c19a85807d702333ec
Kuba Szostko <kubaszostko@gmail.com>
2021-08-21 16:04:14 +0200

    feat: add attraction api

---
+++ travelapp/frontend/src/services/attractionAPI.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8000/api/attractions/';
+
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: `Token ${localStorage.getItem('token')}`,
+};
+
+const create = (attractionData) =>
+  axios
+    .post(BASE_URL, attractionData, {
+      headers,
+    })
+    .then(({ data }) => data)
+    .catch(({ response }) => Promise.reject(response.statusText));
+
+const getByXid = (xid) =>
+  axios
+    .get(`${BASE_URL}?xid=${xid}`, {
+      headers,
+    })
+    .then(({ data }) => data[0])
+    .catch(({ response }) => Promise.reject(response.statusText));
+
+export default { create, getByXid };
--- travelapp/frontend/src/services/index.js
+++ travelapp/frontend/src/services/index.js
@@ -1,4 +1,5 @@
 export { default as UserAPI } from './userAPI';
 export { default as TravelAPI } from './travelAPI';
+export { default as AttractionAPI } from './attractionAPI';
 export { default as PlacesAPI } from './places/placesAPI';
 export { default as RoutingAPI } from './routing/routingAPI';
