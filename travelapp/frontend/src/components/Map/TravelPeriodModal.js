0781db5a19a9c59e035690c287655ebace5a25b2
Kuba Szostko <kubaszostko@gmail.com>
2021-08-22 10:09:36 +0200

    feat(TravelPeriodModal): add steps to complete travel stop creation

--- travelapp/frontend/src/components/Map/TravelPeriodModal.js
+++ travelapp/frontend/src/components/Map/TravelPeriodModal.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Modal, Button } from 'antd';
+import { Modal, Button, Steps } from 'antd';
 import { closeModal } from '../../redux/travelPeriodModal/travelPeriodModalSlice';
+
+const { Step } = Steps;
 
 function TravelPeriodModal() {
   const dispatch = useDispatch();
   const { isOpen } = useSelector((state) => state.travelPeriodModal);
 
+  const [currentStep, setCurrentStep] = useState(0);
+
   function handleCloseModal() {
     dispatch(closeModal());
+  }
+
+  function nextStep() {
+    setCurrentStep((prev) => prev + 1);
+  }
+
+  function prevStep() {
+    setCurrentStep((prev) => prev - 1);
   }
 
   const footer = [
@@ -19,13 +32,23 @@ function TravelPeriodModal() {
   return (
     <Modal
       centered
-      title={<h2>Dodaj do podróży</h2>}
+      title={<h2>Dodaj punkt podróży</h2>}
       visible={isOpen}
       onCancel={handleCloseModal}
       footer={footer}
       width="80vw"
       bodyStyle={{ height: '75vh' }}
-    />
+    >
+      <Steps current={currentStep} size="small" responsive>
+        <Step key="choose-date" title="Data" />
+        <Step key="choose-time" title="Godzina" />
+        <Step key="summary" title="Podsumowanie" />
+      </Steps>
+
+      {currentStep === 0 && <div>Choose date form</div>}
+      {currentStep === 1 && <div>Choose time form</div>}
+      {currentStep === 2 && <div>Choice summary</div>}
+    </Modal>
   );
 }
 
