1db9ce46e1bdfd11f7ef563cbc1c68c61d157c81
Kuba Szostko <kubaszostko@gmail.com>
2021-08-28 15:01:17 +0200

    refactor(Attraction): change max length of two fields

--- travelapp/attractions/models.py
+++ travelapp/attractions/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Attraction(models.Model):
     id = models.AutoField(primary_key=True)
     xid = models.CharField(max_length=50, unique=True)
-    name = models.CharField(max_length=50, unique=True)
-    type = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
+    type = models.CharField(max_length=300)
     lat = models.FloatField()
     lng = models.FloatField()
     description = models.TextField()
