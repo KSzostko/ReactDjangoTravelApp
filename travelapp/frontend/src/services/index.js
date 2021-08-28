27929c5a5e6aa397362089031e1e4b0a0f5729ec
Kuba Szostko <kubaszostko@gmail.com>
2021-08-28 08:56:54 +0200

    feat: create travel stop service

--- travelapp/frontend/src/services/index.js
+++ travelapp/frontend/src/services/index.js
@@ -1,5 +1,6 @@
 export { default as UserAPI } from './userAPI';
 export { default as TravelAPI } from './travelAPI';
+export { default as TravelStopAPI } from './travelStopAPI';
 export { default as AttractionAPI } from './attractionAPI';
 export { default as PlacesAPI } from './places/placesAPI';
 export { default as RoutingAPI } from './routing/routingAPI';
---
+++ travelapp/frontend/src/services/travelStopAPI.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8000/api/travel-stops/';
+
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: `Token ${localStorage.getItem('token')}`,
+};
+
+const create = (travelStopData) =>
+  axios
+    .post(BASE_URL, travelStopData, {
+      headers,
+    })
+    .then(({ data }) => data)
+    .catch(({ response }) => Promise.reject(response.statusText));
+
+const getByTravelId = (travelId) =>
+  axios
+    .get(`${BASE_URL}?travel-id=${travelId}`, {
+      headers,
+    })
+    .then(({ data }) => data)
+    .catch(({ response }) => Promise.reject(response.statusText));
+
+export default { create, getByTravelId };
