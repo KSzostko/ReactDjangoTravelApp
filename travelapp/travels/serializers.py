69b4f49e1444de9d003a166a355c629f4fcada3d
Kuba Szostko <kubaszostko@gmail.com>
2021-08-29 15:34:02 +0200

    refactor(TravelStop): include nested objects instead of sole foreign keys

--- travelapp/travels/serializers.py
+++ travelapp/travels/serializers.py
@@ -18,6 +18,18 @@ class TravelStopSerializer(serializers.ModelSerializer):
     class Meta:
         model = TravelStop
         fields = '__all__'
+        depth = 1
+
+    # probably depth dynamic change is not the most optimal
+    # but it is the quickest working solution for now
+    def __init__(self, *args, **kwargs):
+        super(TravelStopSerializer, self).__init__(*args, **kwargs)
+
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
 
 
 class TravelRouteSerializer(serializers.ModelSerializer):
