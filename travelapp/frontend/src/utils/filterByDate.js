a41b035acde459d0ce39635ee45301204265dc97
Kuba Szostko <kubaszostko@gmail.com>
2021-08-29 17:01:50 +0200

    feat(filterByDate): add sorting

--- travelapp/frontend/src/utils/filterByDate.js
+++ travelapp/frontend/src/utils/filterByDate.js
@@ -1,9 +1,14 @@
-import { isSameDay } from 'date-fns';
+import { isSameDay, compareAsc } from 'date-fns';
 import { parseDate } from './parseDate';
 
 export function filterByDate(items, dateString) {
+  const sortedDates = [...items];
+  sortedDates.sort((a, b) =>
+    compareAsc(new Date(a.start_date), new Date(b.start_date))
+  );
   const parsedDate = parseDate(dateString, 'dd.MM.yyyy');
-  return items.filter((item) =>
+
+  return sortedDates.filter((item) =>
     isSameDay(new Date(item.start_date), parsedDate)
   );
 }
