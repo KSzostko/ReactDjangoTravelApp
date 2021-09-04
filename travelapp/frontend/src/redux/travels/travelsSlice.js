74497d0b83ff95915bbd32c2f8c0aa0516314ee2
Kuba Szostko <kubaszostko@gmail.com>
2021-09-04 15:46:22 +0200

    refactor: remove unused reducer for now

--- travelapp/frontend/src/redux/travels/travelsSlice.js
+++ travelapp/frontend/src/redux/travels/travelsSlice.js
@@ -4,7 +4,6 @@ import { getTravelByIdReducer } from './actions/getTravelById';
 import { getTravelStopsReducer } from './actions/getTravelStops';
 import { addTravelStopReducer } from './actions/addTravelStop';
 import { getTravelRoutesReducer } from './actions/getTravelRoutes';
-import { addTravelRouteReducer } from './actions/addTravelRoute';
 
 const initialState = {
   list: [],
@@ -45,7 +44,6 @@ const travelsSlice = createSlice({
     getTravelStopsReducer(builder);
     addTravelStopReducer(builder);
     getTravelRoutesReducer(builder);
-    addTravelRouteReducer(builder);
   },
 });
 
