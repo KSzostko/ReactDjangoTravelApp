69425d516546647720fe042e6c7fd820a5b9ac75
Kuba Szostko <kubaszostko@gmail.com>
2021-09-29 18:34:54 +0200

    reafctor(TravelStopModal): extract from to a separate component

--- travelapp/frontend/src/components/Map/MapSidebar/TravelStopModal/TravelStopModal.js
+++ travelapp/frontend/src/components/Map/MapSidebar/TravelStopModal/TravelStopModal.js
@@ -1,67 +1,21 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { getHours, getMinutes } from 'date-fns';
 import styled from 'styled-components';
-import { Modal, Form, TimePicker, Button, Spin } from 'antd';
-import moment from 'moment';
-import { timeFormat } from 'setup/constans';
-import { formatHour, useErrorNotification, range, setTimeForDate } from 'utils';
+import { Modal } from 'antd';
 import { closeModal } from 'redux/travelStopModal/travelStopModalSlice';
-import { updateTravelStop } from 'redux/travels/actions/updateTravelStop/thunk';
 import ModalFooter from './ModalFooter';
-
-const { RangePicker } = TimePicker;
+import TravelTimeFrom from './TravelTimeForm';
 
 const ModalTitle = styled.h2`
   font-size: 18px;
   margin-right: 16px;
 `;
 
-const StyledButton = styled(Button)`
-  margin-top: 16px;
-`;
-
 function TravelStopModal() {
   const dispatch = useDispatch();
-  const { isLoading: travelStopsLoading } = useSelector(
-    (state) => state.travels.getTravelStops
-  );
-  const { isOpen, data, earliestTime, latestTime } = useSelector(
-    (state) => state.travelStopModal
-  );
-  const { isLoading: isRouteToStopLoading, error: toStopError } = useSelector(
-    (state) => state.travelStopModal.getRouteToStop
-  );
-  const {
-    isLoading: isRouteFromStopLoading,
-    error: fromStopError,
-  } = useSelector((state) => state.travelStopModal.getRouteFromStop);
 
-  useErrorNotification(toStopError, 'Nie udało się uzyskać danych o drodze');
-  useErrorNotification(fromStopError, 'Nie udało się uzyskać danych o drodze');
+  const { isOpen, data } = useSelector((state) => state.travelStopModal);
 
   function handleCancel() {
-    dispatch(closeModal());
-  }
-
-  async function handleSubmit({ period }) {
-    const [startDate, endDate] = period.map((item) => item.toDate());
-    const updatedStart = setTimeForDate(
-      data.start_date,
-      getHours(startDate),
-      getMinutes(startDate)
-    );
-    const updatedEnd = setTimeForDate(
-      data.end_date,
-      getHours(endDate),
-      getMinutes(endDate)
-    );
-    const updatedStop = {
-      ...data,
-      start_date: updatedStart,
-      end_date: updatedEnd,
-    };
-
-    await dispatch(updateTravelStop(updatedStop));
     dispatch(closeModal());
   }
 
@@ -80,60 +34,8 @@ function TravelStopModal() {
       width="350px"
       bodyStyle={{ height: '220px' }}
     >
-      {isRouteToStopLoading || isRouteFromStopLoading ? (
-        <Spin />
-      ) : (
-        <Form
-          name="travel-stop-form"
-          layout="vertical"
-          size="large"
-          onFinish={handleSubmit}
-          initialValues={{
-            period: [
-              moment(formatHour(data?.start_date), timeFormat),
-              moment(formatHour(data?.end_date), timeFormat),
-            ],
-          }}
-        >
-          <Form.Item
-            name="period"
-            label="Czas zwiedzania"
-            rules={[{ required: true, message: 'Podaj czas zwiedzania' }]}
-          >
-            <RangePicker
-              placeholder={['Start', 'Koniec']}
-              format={timeFormat}
-              disabledHours={() => [
-                ...range(0, earliestTime.hours),
-                ...range(latestTime.hours + 1, 24),
-              ]}
-              disabledMinutes={(selectedHour) => {
-                if (
-                  selectedHour > earliestTime.hours &&
-                  selectedHour < latestTime.hours
-                )
-                  return [];
-
-                if (selectedHour === earliestTime.hours)
-                  return range(0, earliestTime.minutes);
-
-                return range(latestTime.minutes, 60);
-              }}
-            />
-          </Form.Item>
-
-          <Form.Item style={{ color: '#fff' }}>
-            <StyledButton
-              type="primary"
-              htmlType="submit"
-              block
-              loading={travelStopsLoading}
-            >
-              Zatwierdź
-            </StyledButton>
-          </Form.Item>
-        </Form>
-      )}
+      {/* TODO add Steps component */}
+      <TravelTimeFrom />
     </Modal>
   );
 }
---
+++ travelapp/frontend/src/components/Map/MapSidebar/TravelStopModal/TravelTimeForm.js
@@ -0,0 +1,115 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { getHours, getMinutes } from 'date-fns';
+import styled from 'styled-components';
+import { Form, TimePicker, Button, Spin } from 'antd';
+import moment from 'moment';
+import { timeFormat } from 'setup/constans';
+import { formatHour, useErrorNotification, range, setTimeForDate } from 'utils';
+import { closeModal } from 'redux/travelStopModal/travelStopModalSlice';
+import { updateTravelStop } from 'redux/travels/actions/updateTravelStop/thunk';
+
+const { RangePicker } = TimePicker;
+
+const StyledButton = styled(Button)`
+  margin-top: 16px;
+`;
+
+function TravelTimeFrom() {
+  const dispatch = useDispatch();
+
+  const { isLoading: travelStopsLoading } = useSelector(
+    (state) => state.travels.getTravelStops
+  );
+  const { data, earliestTime, latestTime } = useSelector(
+    (state) => state.travelStopModal
+  );
+  const { isLoading: isRouteToStopLoading, error: toStopError } = useSelector(
+    (state) => state.travelStopModal.getRouteToStop
+  );
+  const {
+    isLoading: isRouteFromStopLoading,
+    error: fromStopError,
+  } = useSelector((state) => state.travelStopModal.getRouteFromStop);
+
+  useErrorNotification(toStopError, 'Nie udało się uzyskać danych o drodze');
+  useErrorNotification(fromStopError, 'Nie udało się uzyskać danych o drodze');
+
+  async function handleSubmit({ period }) {
+    const [startDate, endDate] = period.map((item) => item.toDate());
+    const updatedStart = setTimeForDate(
+      data.start_date,
+      getHours(startDate),
+      getMinutes(startDate)
+    );
+    const updatedEnd = setTimeForDate(
+      data.end_date,
+      getHours(endDate),
+      getMinutes(endDate)
+    );
+    const updatedStop = {
+      ...data,
+      start_date: updatedStart,
+      end_date: updatedEnd,
+    };
+
+    await dispatch(updateTravelStop(updatedStop));
+    dispatch(closeModal());
+  }
+
+  if (isRouteToStopLoading || isRouteFromStopLoading) return <Spin />;
+
+  return (
+    <Form
+      name="travel-stop-form"
+      layout="vertical"
+      size="large"
+      onFinish={handleSubmit}
+      initialValues={{
+        period: [
+          moment(formatHour(data?.start_date), timeFormat),
+          moment(formatHour(data?.end_date), timeFormat),
+        ],
+      }}
+    >
+      <Form.Item
+        name="period"
+        label="Czas zwiedzania"
+        rules={[{ required: true, message: 'Podaj czas zwiedzania' }]}
+      >
+        <RangePicker
+          placeholder={['Start', 'Koniec']}
+          format={timeFormat}
+          disabledHours={() => [
+            ...range(0, earliestTime.hours),
+            ...range(latestTime.hours + 1, 24),
+          ]}
+          disabledMinutes={(selectedHour) => {
+            if (
+              selectedHour > earliestTime.hours &&
+              selectedHour < latestTime.hours
+            )
+              return [];
+
+            if (selectedHour === earliestTime.hours)
+              return range(0, earliestTime.minutes);
+
+            return range(latestTime.minutes, 60);
+          }}
+        />
+      </Form.Item>
+
+      <Form.Item style={{ color: '#fff' }}>
+        <StyledButton
+          type="primary"
+          htmlType="submit"
+          block
+          loading={travelStopsLoading}
+        >
+          Zatwierdź
+        </StyledButton>
+      </Form.Item>
+    </Form>
+  );
+}
+
+export default TravelTimeFrom;
