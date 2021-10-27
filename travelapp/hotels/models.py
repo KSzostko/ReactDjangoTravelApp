20eaf31cff8ceaa724a4b58f6ea4082eebd83fae
Kuba Szostko <kubaszostko@gmail.com>
2021-10-27 16:45:21 +0200

    feat(Hotel): remove 2 fields from model

--- travelapp/hotels/models.py
+++ travelapp/hotels/models.py
@@ -5,11 +5,9 @@ class Hotel(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40)
     address = models.CharField(max_length=100)
-    description = models.TextField()
     lat = models.FloatField()
     lng = models.FloatField()
     stars = models.IntegerField()
-    photo = models.ImageField(upload_to='hotels/', default='no-photo-available.png')
 
     def __str__(self):
         return f'Hotel {self.name} with {self.stars} stars'
