55ba05ba5433271612eb08d34b5a3dd7d213c9f6
Kuba Szostko <kubaszostko@gmail.com>
2021-10-31 10:48:00 +0100

    test: check unauthenticated app routing

--- travelapp/frontend/src/setupTests.js
+++ travelapp/frontend/src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// this solves problem with testing ant design components with rtl
+// https://stackoverflow.com/questions/64813447/cannot-read-property-addlistener-of-undefined-react-testing-library
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    };
+  };
