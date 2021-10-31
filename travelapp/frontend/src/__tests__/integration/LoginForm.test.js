5c71f2af11cb09968f7b9e03cdad068e717256b0
Kuba Szostko <kubaszostko@gmail.com>
2021-10-31 12:38:35 +0100

    test(LoginForm): check validation

--- travelapp/frontend/src/__tests__/integration/LoginForm.test.js
+++ travelapp/frontend/src/__tests__/integration/LoginForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent, waitFor } from '@testing-library/react';
 import { render } from 'utils/testUtils';
 import LoginForm from 'components/LoginForm';
 
@@ -9,5 +9,15 @@ describe('<LoginForm />', () => {
 
     expect(screen.getByLabelText('Login')).toBeInTheDocument();
     expect(screen.getByLabelText('Hasło')).toBeInTheDocument();
+  });
+
+  it('shows error messages when form is not filled', async () => {
+    render(<LoginForm />);
+
+    fireEvent.click(screen.getByText('Zaloguj się'));
+    await waitFor(() =>
+      expect(screen.getByText('Podaj nazwę użytkownika')).toBeInTheDocument()
+    );
+    expect(screen.getByText('Podaj poprawne hasło')).toBeInTheDocument();
   });
 });
