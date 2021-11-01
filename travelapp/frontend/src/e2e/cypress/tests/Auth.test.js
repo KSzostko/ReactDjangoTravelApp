f27fade7a2e49696b3ca130322e3f3371423490c
Kuba Szostko <kubaszostko@gmail.com>
2021-11-01 11:23:06 +0100

    test(Auth): check login

--- travelapp/frontend/src/e2e/cypress/tests/Auth.test.js
+++ travelapp/frontend/src/e2e/cypress/tests/Auth.test.js
@@ -7,4 +7,16 @@ describe('Authentication', () => {
     cy.visit('/photos');
     cy.findByText('Zaloguj się').should('exist');
   });
+
+  it('redirects user to the main page after login', () => {
+    cy.visit('/');
+
+    cy.findByLabelText('Login').type('testuser');
+    cy.findByLabelText('Hasło').type('testpassword');
+    cy.findAllByText('Zaloguj się').click();
+
+    cy.findByText('Pomyślne logowanie').should('exist');
+    cy.findByText('Witamy ponownie!').should('exist');
+    cy.findByText('Podróże').should('exist');
+  });
 });
